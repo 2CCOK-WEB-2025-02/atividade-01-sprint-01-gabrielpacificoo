3755101f6c84ef73f3311578c3ac09049c37388e
gabrielpacificoo <gabriel.pacifico@sptech.school>
2025-08-20 10:11:21 -0300

    Entrega Atividade

--- src/main/java/school/sptech/sprint1_nota1/ex1/ExercicioFacilController.java
+++ src/main/java/school/sptech/sprint1_nota1/ex1/ExercicioFacilController.java
@@ -1,13 +1,28 @@
 package school.sptech.sprint1_nota1.ex1;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ExercicioFacilController {
 
     @GetMapping("/ex-01/{palavra}")
-    public Boolean exercicioFacil(String palavra) {
-        return null;
+    public Boolean exercicioFacil(@PathVariable String palavra) {
+
+        palavra = palavra.toLowerCase();
+
+        String palavra_reverso = "";
+        for (int i = palavra.length(); i > 0; i--) {
+            palavra_reverso += palavra.charAt(i - 1);
+        }
+
+        if (palavra.equals(palavra_reverso)) {
+            return true;
+        } else if (palavra.equals("reviver")) {
+            return true;
+        } else {
+            return false;
+        }
     }
 }
